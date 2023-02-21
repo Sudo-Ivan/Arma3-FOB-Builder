f20114c27483b1feef76cea60bfec1840e64613a
Ivan Ryan <contact@ivanryan.slmail.me>
2023-02-21 05:38:14 -0600

    add: settings

--- shared/builder.hpp
+++ shared/builder.hpp
@@ -1,5 +1,7 @@
-// These are the stored parameter IDs, and should be named BUILDER_PARAM_<param name>
-// * They do NOT necessarily correspond to the parameter index in the array
-// * They are opaque identifiers
-// * The name of the ID does not matter, but you should follow the convention above
-// * WARNING: Do not reorder or reuse ID numbers
+// buildBoxes = [portablebuildBox1, portablebuildBox2];
+
+// POINTS_FACTIONS = [west, east, guer];
+
+// STARTING_POINTS_WEST = 
+// STARTING_POINTS_EAST = 
+// STARTING_POINTS_GUER = 
